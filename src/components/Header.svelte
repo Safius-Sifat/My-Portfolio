5407f2c3ca16888549c4d4a01b9de5009c9e6802
Safius Sifat <71771857+Safius-Sifat@users.noreply.github.com>
2023-12-23 16:50:38 +0000

    Add layout and page components-1

---
+++ src/components/Header.svelte
@@ -0,0 +1,27 @@
+<script>
+    export let y;
+    let tabs = [
+        {
+            name: 'Projects',
+            link: '#projects'
+        },
+        {
+            name: 'About me',
+            link: '#about'
+        },
+       
+    ];
+</script>
+
+<header>
+    <h1>
+        <b>Safius</b> Sifat
+    </h1>
+    <div class="">
+        {#each tabs as tab,index}
+            <a >
+                <p>{tab.name}</p>
+            </a>
+        {/each}
+    </div>
+</header>
